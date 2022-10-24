d33876b367b10856bd059c22f2da9c7629a51d6c
MarcoDevelop <m.miranda2015@outlook.com>
2022-10-24 17:27:40 -0300

    Setup do projeto

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#f9fafb',
+
+  'gray-100': '#f3f4f6',
+  'gray-300': '#cbd5e1',
+  'gray-400': '#9ca3af',
+  'gray-500': '#6b7280',
+  'gray-600': '#4b5563',
+  'gray-700': '#334155',
+  'gray-800': '#1f2937',
+  'gray-900': '#111827',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-300': '#fca5a5',
+  'red-500': '#ef4444',
+  'red-700': '#b91c1c',
+} as const
