54583748ae38dd681a8a9a3d086ae1361e196f5d
MarcoDevelop <m.miranda2015@outlook.com>
2022-10-24 20:07:03 -0300

    Componente: Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { HeaderContainer, HeaderContent, NewTransactionsButton } from './styles'
+
+import logoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
+        <NewTransactionsButton>Nova Transação</NewTransactionsButton>
+      </HeaderContent>
+    </HeaderContainer>
+  )
+}
